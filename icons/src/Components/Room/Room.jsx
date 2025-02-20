0ebc0142a25d5eb84bc33f6a3a63543da68f9bf4
dishanayak383 <dishanayak383@gmail.com>
2025-02-20 11:03:05 +0530

    first commit

--- icons/src/Components/Room/Room.jsx
+++ icons/src/Components/Room/Room.jsx
@@ -5,45 +5,73 @@ export const Room=()=>{
     return(
         <div style={{border:'1px solid black',width:'450px',height:'800px',display:'flex',flexDirection:'column',}}>
             <div style={{display:'flex',border:'1px solid black',flex:3,flexDirection:'column',}}>
-                <div style={{display:'flex',border:'1px solid black',flex:1,flexDirection:'row',}}>
+                <div style={{display:'flex',border:'1px solid black',flex:0.5,flexDirection:'row',}}>
                     <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',borderRadius:'400px',margin:'5px',marginLeft:'10px',marginRight:'80px',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',borderRadius:'40px',margin:'5px',marginLeft:'10px',marginRight:'40px',}}></div>
                     <div style={{display:'flex',border:'1px solid black',flex:'6',flexDirection:'column',}}>
                         <a>Joana Barrors</a>
                         <p>Hotel Guest</p>
                     </div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',borderRadius:'40px',margin:'5px',}}></div>
                     <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
                 </div>
-                <div style={{display:'flex',border:'1px solid black',flex:0.5,flexDirection:'row',}}></div>
+                <div style={{display:'flex',border:'1px solid black',flex:0.5,flexDirection:'row',}}>
+                    <a>  Recommended for you  </a>
+                </div>
                 <div style={{display:'flex',border:'1px solid black',flex:1.5,flexDirection:'row',}}>
                     <div style={{display:'flex',border:'1px soild black',flex:'3',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'6',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}>
+                        <a>Enjoy some me time</a>
+                        <a>Relax with our messages and treatments.</a>
+                        <a>Spa facilities will close in 4 hours</a>
+                    </div>
                 </div>
                 <div style={{display:'flex',border:'1px solid black',flex:0.5,flexDirection:'row',}}></div>
 
             </div>
             <div style={{display:'flex',border:'1px solid black',flex:6,flexDirection:'column',}}>
-                <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}></div>
+                <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}>
+                    <a>Popular experiences</a>
+                </div>
                 <div style={{display:'flex',border:'1px solid black',flex:'3',flexDirection:'row',}}>
-                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}>
+                        <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',}}></div>
+                        <div style={{display:'flex',border:'1px solid black',flex:'5',flexDirection:'column',}}>
+                            <p>Bike Riding</p>
+                            <a>Varied tour options</a>
+                        </div>
+                    </div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}>
+                        <div style={{display:'flex',border:'1px solid black',flex:'2',flexDirection:'column',}}></div>
+                        <div style={{display:'flex',border:'1px solid black',flex:'5',flexDirection:'column',}}>
+                            <p>Dinner at the Table</p>
+                            <a>From $30/person</a>
+                        </div>
+                    </div>
                 </div>
                 <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}></div>
-                <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}></div>
-                <div style={{display:'flex',border:'1px solid black',flex:'1.5',flexDirection:'row',}}>
-                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
+                <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'row',}}>
+                    <p>All services</p>
                 </div>
                 <div style={{display:'flex',border:'1px solid black',flex:'1.5',flexDirection:'row',}}>
                     <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}>
+                        <p>Facilities</p>
+                    </div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',borderRadius:'40px',margin:'5px',}}></div>
                 </div>
                 <div style={{display:'flex',border:'1px solid black',flex:'1.5',flexDirection:'row',}}>
                     <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
-                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}>
+                        <p>Virtual Key</p>
+                    </div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',borderRadius:'40px',margin:'5px',}}></div>
+                </div>
+                <div style={{display:'flex',border:'1px solid black',flex:'1.5',flexDirection:'row',}}>
+                    <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
+                    <div style={{display:'flex',border:'1px solid black',flex:'4',flexDirection:'column',}}>
+                        <p>My Room</p>
+                    </div>
                     <div style={{display:'flex',border:'1px solid black',flex:'1',flexDirection:'column',}}></div>
                 </div>
                 <div style={{display:'flex',border:'1px solid black',flex:'1.5',flexDirection:'row',}}>
